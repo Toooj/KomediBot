cd20df3fc714e9483b9b4b7d893c3f3b8ced1d83
Toooj <107245290+Toooj@users.noreply.github.com>
2022-06-20 13:21:21 +0200

    fixed randomized stuff bc the cooper way didnt work correctly

--- bot.py
+++ bot.py
@@ -45,83 +45,104 @@ async def on_message(message):
     
 #####################
 
-# Fun stuff
+# Fun Stuff
 
 #####################
-
-
 
     if message.content == '~quote':
         rng = random.randrange(0,len(list))
         response = list[rng]['text']
         await message.channel.send(response)
 
-    elif message.content.find('LMAO')>=0:
+    elif message.content.find('LMAO')>=0 or message.content.find('LMFAO')>=0 or message.content.find('LFMAO')>=0:
         numKomedi = message.content.count('O')
         response = numKomedi*'KOMEDI '
         await message.channel.send(response)
        
     elif message.content[0:5] == '~r6op':
+        
+        attackerList = komedi.attackerList
+        defenderList = komedi.defenderList
+        random.shuffle(attackerList)
+        random.shuffle(defenderList)
+
         I=1
         if message.content[-1].isnumeric() == True:
             if int(message.content[-1]) < 1 or int(message.content[-1]) > 5:
                 await message.channel.send('Must be 0 < n <= 5')
             I = int(message.content[-1])
  
-        random.shuffle(komedi.attackerList)
-        random.shuffle(komedi.defenderList)
-
         response=''
         for item in komedi.atkAliases:
             if message.content.find(item) >= 0:
                 for i in range(I):
-                    response+=komedi.attackerList[i]+' '
+                    response+=attackerList[i]+' '
                 await message.channel.send(response)
                 break
             
         for item in komedi.defAliases:
             if message.content.find(item) >= 0:
                 for i in range(I):
-                    response+=komedi.defenderList[i]+' '
+                    response+=defenderList[i]+' '
                 if response.find('~~Clash~~') >= 0:
-                    response+=komedi.defenderList[-1]+' '
+                    response+=defenderList[-1]+' '
                 await message.channel.send(response)
                 break
 
     elif message.content == '~conspiracy':
-        #not actually sure if this will work, but it shouldn't be hard to find a replacement
-        #just random a number and use that as the var
-        #might need to move the conspiracies list back to this file
-        random.shuffle(komedi.organizations)
-        random.shuffle(komedi.firstNames)
-        random.shuffle(komedi.lastNames)
-        random.shuffle(komedi.existingPeople)  
-        random.shuffle(komedi.countries)
-        random.shuffle(komedi.places)
-        random.shuffle(komedi.religions)
-        random.shuffle(komedi.events)
-        random.shuffle(komedi.actions)
-
-        random.shuffle(komedi.conspiracies)
         
-        komedi.conspiracies[0].capitalize()
-        await message.channel.send(komedi.conspiracies[0])
+        organizations = komedi.organizations
+        firstNames = komedi.firstNames
+        lastNames = komedi.lastNames
+        existingPeopleAlive = komedi.existingPeopleAlive
+        existingPeopleDead = komedi.existingPeopleDead
+        existingPeopleFictional = komedi.existingPeopleFictional
+        existingPeopleALL = komedi.existingPeopleALL
+        countries = komedi.countries
+        places = komedi.places
+        religions = komedi.religions
+        events = komedi.events
+        actions = komedi.actions
+        
+        random.shuffle(organizations)
+        random.shuffle(firstNames)
+        random.shuffle(lastNames)
+        random.shuffle(existingPeopleAlive)
+        random.shuffle(existingPeopleDead)
+        random.shuffle(existingPeopleFictional)
+        random.shuffle(existingPeopleALL)
+        random.shuffle(countries)
+        random.shuffle(places)
+        random.shuffle(religions)
+        random.shuffle(events)
+        random.shuffle(actions)
+        
+        ### these things need to be randomized every time ~conspiracy is said
+
+        conspiracyInputs=[organizations,firstNames,lastNames,existingPeopleAlive,existingPeopleDead,existingPeopleFictional,existingPeopleALL,countries,places,religions,events,actions]
+        conspiracies=komedi.Conspiracy(*conspiracyInputs)
+        random.shuffle(conspiracies)
+
+        conspiracies[0].capitalize()
+        await message.channel.send(conspiracies[0])
         
 
-
-
-    ### clean this shit up with functions or something
+    ### clean this shit up with functions or something?
 
 
 #####################
 
-# Game Stuff
+# RuneScape Stuff
 
 #####################
 
     elif message.content[0:9] == '~osrswiki':
 
         await message.channel.send('https:// ill finish this later lol') ### do this 
+
+    elif message.content[0:7] == '~osrsge':
+
+        await message.channel.send('https://') ### do this
     
 
     elif message.content[0:8] == '~rs3wiki':
@@ -137,6 +158,7 @@ async def on_message(message):
 #####################
 
     elif message.content == '~siege':
+
         await message.channel.send('Do not attempt to board the helicopter. إنشالله الله أكبر أستغفرلله. أنت قرد غبي وستأكل حذائي. @Rainbow 6: Siege') #figure out how to tag people lmfao
                                         
 
@@ -146,8 +168,29 @@ async def on_message(message):
 
 #####################
 
-    elif message.content == '~help':
-        await message.channel.send('fuck you (for now) ill fill this in later lol')
+    elif message.content[0:5]=='~help':
+
+        response = '**AVAILABLE COMMANDS:**\n\n'
+        response+= '\n**Fun Stuff:**\n\n'
+        response+= '**~quote** : Generates a random quote from the BBB Twitter @BBBQuotes1.\n'
+        response+= '**~conspiracy** : Generates random conspiracy theory.\n'
+        response+= '**~r6op <team> <number>** : Generates <number> operators from <team> (i.e. attack/defence).\n'
+        response+= '**~roll <numdice><typedice>** : NOT IMPLEMENTED\n' ### maybe in the future might be worth making a dedicated DnD set of commands?
+        response+= '\n**RuneScape Things:**\n\n'
+        response+= '**~osrswiki <item>** : NOT IMPLEMENTED\n'
+        response+= '**~osrsge <item>** : NOT IMPLEMENTED\n'
+        response+= '**~rs3wiki <item>** : NOT IMPLEMENTED\n'
+        response+= '\n**Game Summons:**\n\n'
+        response+= '**~siege** : Tags the Rainbow 6: Siege role.\n'
+
+        if message.content.find('FULL')>=0:
+
+            response+= '\n\n**PASSIVE FUNCTIONS:**\n\n'
+            response+= '- Responds to LMAO variants with "KOMEDI" as many times as there are Os.\n'
+            response+= '- Automatically converts and messages timezone based on timezone role of sender. NOT IMPLEMENTED\n'
+
+        await message.channel.send('Check your DMs <:fuckboi:988374504168390717>') #fuckboi emoji
+        await message.author.send(response)
 
 
 #######################################################################################################
--- komedi.py
+++ komedi.py
@@ -1,5 +1,6 @@
+atkAliases = ['att','atk','attack','attacker','a']
+defAliases = ['def','defender','defence','defense','d']
 
-#fixed ur mong formatting
 attackerList = ['Sledge','Thatcher', 'Ash','Thermite','Twitch',
                 'Montagne', 'Glaz', 'Fuze', 'Blitz', 'IQ', 
                 'Buck', 'Blackbeard', 'Capitao', 'Hibana','Jackal',
@@ -16,38 +17,39 @@ defenderList = ['Smoke','Mute','Castle','Pulse','Doc',
                 'Wamai','Oryx','Melusi','Aruni','Thunderbird',
                 'Thorn','Azami']
 
-
-atkAliases = ['att','atk','attack','attacker','a']
-defAliases = ['def','defender','defence','defense','d']
-
-
 organizations = ['Amazon','NASA','FedEx','Disney','Fox','Netflix',
                 'Google','Facebook','Snapchat','the Church', 
                 'Government of North Korea','NATO','Ubisoft',
-                'the Illuminati','Antifa','QAnon']
-
+                'the Illuminati','Antifa','QAnon','the Ku Klux Klan']
 
 firstNames = ['Ryan','Gerald','Abhilash','Biswajit','Xaofan',
               'Hideshi','Tom','Richard','Harry','Martin','Anton','Kirill',
-              'Phil','William','Alen','Max','Jeffery',
-              'Nigel','Mohammed','Magnus','Josiah','Ezekiel',
-              'Hannah','Hillary',
+              'Phil','William','Alen','Max','Jeffery','Stanislav','Vladimir',
+              'Nigel','Mohammed','Magnus','Josiah','Ezekiel','Elena','Kyle',
+              'Hannah','Hillary','Cooper','Jesse','Tejas','Andrew','Lewis',
               'Shaniqua','Jamal','Tyrone']
 
 lastNames = ['Biggums','Cox','Sharath','Shekelstein','Chang','Hussein',
-            'Imam','bin Laden','Nakamoto','Patel','Kumar',
-             'Singh','Karlson','Putin','Bezos','Bernstein',
+            'Imam','bin Laden','Nakamoto','Patel','Kumar','Woods','Shah',
+             'Singh','Karlson','Putin','Bezos','Bernstein','Goldstein',
              'Epstein','Clinton','Jameson','Roberts','Hilberts']
 
-existingPeople = ['Jeff Bezos','Elon Musk','Barack Obama','Osama bin Laden','Hillary Clinton',
-                  'George Soros','Walt Disney','Adolf Hitler','Anne Frank','Bill Gates',
-                  'Hermes Trismegistus','Aristotle','Bill Clinton','Donald Trump',
-                  'Mike Pence','Plato','Abraham Lincoln','Satan','Jesus',
-                  'Aleister Crowley','Rabbi Menachem Mendel Schneerson']
+existingPeopleAlive = ['Jeff Bezos','Elon Musk','Barack Obama','Hillary Clinton',
+                  'George Soros','Bill Gates','Mike Pence','Bill Clinton','Donald Trump',
+                  'Jair Bolsonaro','Eminem']
+
+existingPeopleDead = ['Osama bin Laden','Isaac Newton','Tupac','Walt Disney','Adolf Hitler','Anne Frank','Aristotle',
+                      'Plato','Abraham Lincoln','Aleister Crowley','Rabbi Menachem Mendel Schneerson','Biggie']
+
+existingPeopleFictional = ['Jesus Christ','Satan','God','Prophet Mohammed','Moses','Gautama Buddha','Shrek',
+                           'King Roald']
+
+existingPeopleALL = existingPeopleAlive + existingPeopleDead + existingPeopleFictional
 
 countries = ['India','USA','South Korea','Russia','Norway','Germany','UK',
-             'Ireland','Australia','Argentina','Brazil',
-             'Azerbaijan','Austria','Bahrain','Albania','Armenia']
+             'Ireland','Australia','Argentina','Brazil','the Netherlands',
+             'Azerbaijan','Austria','Bahrain','Albania','Armenia','Sweden',
+             'Thailand','Vietnam','New Zealand','Somalia','Ecuador','Peru']
 
 places = ['London','Vienna','Dhaka','Moscow','Yerevan','Tamil Nadu','the Moon','Mars']
 
@@ -55,18 +57,25 @@ religions = ['Islam','Hinduism','Buddhism','Catholicism','Zoroastrianism','Satan
              'Judaism','Orthodox Christianity',
              'Atheism',"Baha'i",'Confucianism','Druze','Jainism','Rastafarianism','Sikhism']
 
-events = ['the crucifixion of Jesus','9/11','Uvalde','WW2','The Holocaust']
-actions = ['False Flags','Child Sacrifice','Public Masturbation','Mass Rape','Genocide',
-           'Selective Breeding','Narco-Terrorism','Societal Degeneracy']
+events = ['the crucifixion of Jesus Christ','9/11','Uvalde','WW2','the Holocaust','the Trump Presidency']
 
-conspiracies = [events[0]+' was caused by '+existingPeople[0]+' after going back in time using the secret facility in '+countries[0]+', run by '+organizations[0], 
-                existingPeople[0]+' is dead... replaced by a robot from '+countries[0]+' named '+firstNames[0]+' '+lastNames[0],
-                existingPeople[0]+' burns effigy of '+existingPeople[1]+' for glory of '+religions[0]+' in '+countries[0],
-                religions[0]+' was actually founded in 1988 by '+firstNames[0]+' '+lastNames[0],
-                existingPeople[0]+' is secretly funding '+actions[0]+" to appease Moloch's High Priest, "+existingPeople[1],
-                existingPeople[0]+' is actively trying to cover up '+events[0]+' with disinformation via main stream media',
-                existingPeople[0]+' was actually born in 49AD, and has been repeatedly cloned by '+existingPeople[1]+', in '+places[0]+', to maintain the bloodline of Jesus Christ',
-                existingPeople[0]+' is actually dead',
-                existingPeople[0]+' is actually alive',
-                existingPeople[0]+' and '+existingPeople[1]+' are actually the same person'
-                ]
+actions = ['false flags','child sacrifice','public masturbation','mass rape','genocide',
+           'selective breeding','Narco-Terrorism','societal degeneracy','Pride Month','circumcision',
+           'abortion','racism']
+
+def Conspiracy(organizations,firstNames,lastNames,existingPeopleAlive,existingPeopleDead,existingPeopleFictional,existingPeopleALL,countries,places,religions,events,actions):
+
+    conspiracies = [events[0]+' was caused by '+existingPeopleAlive[0]+' after going back in time using the secret facility in '+countries[0]+', run by '+organizations[0], 
+                            existingPeopleAlive[0]+' is dead... replaced by a robot from '+countries[0]+' named '+firstNames[0]+' '+lastNames[0],
+                            existingPeopleALL[0]+' burns effigy of '+existingPeopleALL[1]+' for the glory of '+religions[0]+' in '+countries[0],
+                            religions[0]+' was actually founded in 1988 by a person named '+firstNames[0]+' '+lastNames[0],
+                            existingPeopleAlive[0]+' is secretly funding '+actions[0]+" to appease Moloch's High Priest, "+existingPeopleAlive[1],
+                            existingPeopleAlive[0]+' is actively trying to cover up '+events[0]+' with disinformation via main stream media',
+                            existingPeopleALL[0]+' was actually born in 49AD, and has been repeatedly cloned by '+existingPeopleALL[1]+', in '+places[0]+', to maintain the bloodline of '+existingPeopleFictional[0],
+                            existingPeopleAlive[0]+' is actually dead',
+                            existingPeopleDead[0]+' is actually alive',
+                            existingPeopleALL[0]+' is actually gay',
+                            existingPeopleALL[0]+' and '+existingPeopleALL[1]+' are actually the same person'
+                            ]
+
+    return conspiracies
