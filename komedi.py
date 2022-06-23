8c0ea84c93798f31478875f70090559feaa13a76
Toooj <107245290+Toooj@users.noreply.github.com>
2022-06-23 21:30:04 +0200

    added to conspiracies

--- komedi.py
+++ komedi.py
@@ -20,38 +20,46 @@ defenderList = ['Smoke','Mute','Castle','Pulse','Doc',
 organizations = ['Amazon','NASA','FedEx','Disney','Fox','Netflix',
                 'Google','Facebook','Snapchat','the Church', 
                 'Government of North Korea','NATO','Ubisoft',
-                'the Illuminati','Antifa','QAnon','the Ku Klux Klan']
+                'the Illuminati','Antifa','QAnon','the Ku Klux Klan', 'Grubhub',
+                'Doordash','Instacart','TSA','NSA','US Supreme Court']
 
 firstNames = ['Ryan','Gerald','Abhilash','Biswajit','Xaofan',
               'Hideshi','Tom','Richard','Harry','Martin','Anton','Kirill',
               'Phil','William','Alen','Max','Jeffery','Stanislav','Vladimir',
               'Nigel','Mohammed','Magnus','Josiah','Ezekiel','Elena','Kyle',
               'Hannah','Hillary','Cooper','Jesse','Tejas','Andrew','Lewis',
-              'Shaniqua','Jamal','Tyrone']
+              'Shaniqua','Jamal','Tyrone','Wei-Yu','Hitori','Johnathan']
 
 lastNames = ['Biggums','Cox','Sharath','Shekelstein','Chang','Hussein',
             'Imam','bin Laden','Nakamoto','Patel','Kumar','Woods','Shah',
              'Singh','Karlson','Putin','Bezos','Bernstein','Goldstein',
-             'Epstein','Clinton','Jameson','Roberts','Hilberts']
+             'Epstein','Clinton','Jameson','Roberts','Hilberts','Einstein',
+             'Zhang','Crews','Timonin','Trump','Clinton','Obama']
 
 existingPeopleAlive = ['Jeff Bezos','Elon Musk','Barack Obama','Hillary Clinton',
                   'George Soros','Bill Gates','Mike Pence','Bill Clinton','Donald Trump',
-                  'Jair Bolsonaro','Eminem']
+                  'Jair Bolsonaro','Eminem','Boris Johnson','Queen Elizabeth II']
 
 existingPeopleDead = ['Osama bin Laden','Isaac Newton','Tupac','Walt Disney','Adolf Hitler','Anne Frank','Aristotle',
-                      'Plato','Abraham Lincoln','Aleister Crowley','Rabbi Menachem Mendel Schneerson','Biggie']
+                      'Plato','Abraham Lincoln','Aleister Crowley','Rabbi Menachem Mendel Schneerson','Biggie', 'Julius Caesar',
+                      'Emperor Nero']
 
 existingPeopleFictional = ['Jesus Christ','Satan','God','Prophet Mohammed','Moses','Gautama Buddha','Shrek',
-                           'King Roald']
+                           'King Roald','Abraham','Gilgamesh','Allah']
 
 existingPeopleALL = existingPeopleAlive + existingPeopleDead + existingPeopleFictional
 
 countries = ['India','USA','South Korea','Russia','Norway','Germany','UK',
              'Ireland','Australia','Argentina','Brazil','the Netherlands',
              'Azerbaijan','Austria','Bahrain','Albania','Armenia','Sweden',
-             'Thailand','Vietnam','New Zealand','Somalia','Ecuador','Peru']
+             'Thailand','Vietnam','New Zealand','Somalia','Ecuador','Peru',
+             'Finland','China','Mongolia','Italy','Chad','Niger','Nigeria',
+             'South Africa','Belgium','France','Spain','Uzbekistan','Turkey',
+             'Kazakhstan','Turkmenistan','Pakistan','Nepal','Sri Lanka']
 
-places = ['London','Vienna','Dhaka','Moscow','Yerevan','Tamil Nadu','the Moon','Mars']
+places = ['London','Vienna','Dhaka','Moscow','Yerevan','Tamil Nadu','the Moon','Mars',
+          'Bangalore','San Francisco','Los Angeles','Glendale','Denver','New York','Antwerp',
+          'Amsterdam','Paris','Berlin','Kiev','San Diego','Vancouver','Montreal']
 
 religions = ['Islam','Hinduism','Buddhism','Catholicism','Zoroastrianism','Satanism','Thelema',
              'Judaism','Orthodox Christianity',
@@ -76,7 +84,13 @@ def Conspiracy(organizations,firstNames,lastNames,existingPeopleAlive,existingPe
                             existingPeopleDead[0]+' is actually alive',
                             existingPeopleALL[0]+' is actually gay',
                             existingPeopleALL[0]+' and '+existingPeopleALL[1]+' are actually the same person',
-                            existingPeopleAlive[0]+' actually won the 2020 election'
+                            existingPeopleAlive[0]+' actually won the 2020 election',
+                            'Mass surveillance via 5G is being rolled out in '+countries[0]+' at the behest of '+existingPeopleAlive[0]+' because of the events of '+events[0],
+                            'In order to counter the spread of '+religions[0]+', '+religions[1]+' has contracted the services of '+organizations[0]+'.',
+                            religions[0]+' secretly reveres '+actions[0],
+                            'The second coming of '+existingPeopleFictional[0]+' will come when people start to believe in the (clearly falsified) events of '+events[0],
+                            organizations[0]+' and '+organizations[1]+' are working closely to disprove the existence of '+existingPeopleFictional[0],
+                            countries[0]+' does not actually exist. Come on, do you know anyone from '+countries[1]+'?'
                             ]
 
     return conspiracies
