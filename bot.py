276bcf2389027654802825ea1d465fe6a0fa4730
Toooj <107245290+Toooj@users.noreply.github.com>
2022-06-24 15:07:01 +0200

    restructuring and new function placeholders

--- bot.py
+++ bot.py
@@ -7,7 +7,6 @@ import random
 import tweepy
 import discord
 from discord.ext import commands
-
 import roles
 import komedi
 
@@ -44,23 +43,14 @@ KomediBot = commands.Bot(command_prefix = '~', help_command=None)
 async def on_ready():
     print(f'KomediBot has connected to Discord!')
 
-# Actual stuff ########################################################################################
+# CHAT RESPONSE ########################################################################################
 
 @KomediBot.listen()
 async def on_message(message):
-
     if message.author.id == KomediBot.user.id:         #no mirrors allowed
        return
 
-    
-
-#####################
-
-# Game Summons
-
-#####################
-
-    elif message.content[0] == '~':
+    elif message.content[0] == '~':                     #Game Summons & other custom commands
         for alias in roles.gameSummonAliases:
             if alias == message.content[1:len(alias)+1]:
 
@@ -79,16 +69,48 @@ async def on_message(message):
             await message.channel.send(response)
             break
 
+# COMMANDS #############################################################################################
+
+# Music #
 
 @KomediBot.command()
-async def quote(ctx):
+async def p(ctx): ## TODO
+    pass
 
-    random.shuffle(quoteList)
-    await ctx.send(quoteList[0]['text'])
+@KomediBot.command()
+async def s(ctx): ## TODO
+    pass
+
+@KomediBot.command()
+async def dc(ctx): ## TODO
+    pass
+
+@KomediBot.command()
+async def showq(ctx): ## TODO
+    pass
+
+@KomediBot.command()
+async def addq(ctx): ## TODO
+    pass
+
+@KomediBot.command()
+async def clrq(ctx): ## TODO
+    pass
+
+@KomediBot.command()
+async def play(ctx): ## same as play
+    #p(ctx)
+    pass
+    
+@KomediBot.command()
+async def skip(ctx): ## same as skip
+    #s(ctx)
+    pass
+
+# Siege #
 
 @KomediBot.command()
 async def r6op(ctx, team, number=1):
-    
     response = '~r6op syntax is: `~r6op <team> <number>`\n'
     if int(number) not in range(1,6):
         response+= '<number> must be between 1 and 5 (inclusive)'
@@ -115,6 +137,78 @@ async def r6op(ctx, team, number=1):
             if response.find('~~Clash~~') >= 0:
                 response+=defenderList[-1]+' '
             await ctx.send(response)
+
+@KomediBot.command()
+async def r(ctx): ## TODO
+    print('test')
+    await ctx.send('test')
+
+@KomediBot.command()
+async def roll(ctx):
+    #r(ctx)
+    pass
+
+# RuneScape #
+
+@KomediBot.command()
+async def osrswiki(ctx):
+    await ctx.send('osrs wiki page for item in ~osrswiki <item>') ## TODO
+
+@KomediBot.command()
+async def osrsge(ctx):
+    await ctx.send('osrs ge price/data for item in ~osrswiki <item>') ## TODO
+
+@KomediBot.command()
+async def rs3wiki(ctx):
+    await ctx.send('rs3 wiki page for item in ~rs3wiki <item>') ## TODO
+    
+# Misc #
+
+@KomediBot.command()
+async def quote(ctx):
+    random.shuffle(quoteList)
+    await ctx.send(quoteList[0]['text'])
+
+
+@KomediBot.command()
+async def learn(ctx, command, role, flavortext):
+    if command in (LIST_OF_COMMANDS + roles.gameSummonAliases):
+        await ctx.send('That command already exists')
+
+    elif role in roles.roleIDs:
+        await ctx.send('A command for this role already exists')
+
+    else:
+        await ctx.send("I'm a stupid robot who can't figure out how long the flavortext is if you don't put it in quotations")
+        
+        gameSummonAliases = roles.gameSummonAliases
+        roleIDs = roles.roleIDs
+        existingflavortext = roles.flavortext
+
+        os.rename('roles.py','roles_ifthisexistssomethingwentwrong.py')
+
+        gameSummonAliases.append(str(command))
+        roleIDs.append(str(role))
+        existingflavortext.append(str(flavortext))
+
+        f = open('roles.py','x')
+        f.write('gameSummonAliases = ["' + '", "'.join([str(item) for item in gameSummonAliases]) + '"]')
+        f.write('\n\n')
+        f.write('roleIDs = ["' + '", "'.join([str(item) for item in roleIDs]) + '"]')
+        f.write('\n')
+        f.write('flavortext = ["' + '", "'.join([str(item) for item in existingflavortext]) + '"]')
+        f.write('\n\n')
+        f.write('gameSummonData = list(zip(roleIDs,flavortext))')
+        f.write('\n\n')
+        f.write('gameSummonDict = dict(zip(gameSummonAliases,gameSummonData))')
+        f.close()
+
+        importlib.reload(roles)
+
+        os.remove('roles_old.py')
+        os.rename('roles_ifthisexistssomethingwentwrong.py','roles_old.py')
+
+        await ctx.send('Me smart now, me know now what ~'+str(roles.gameSummonAliases[-1]) +' means')
 
 @KomediBot.command()
 async def conspiracy(ctx):
@@ -154,102 +248,31 @@ async def conspiracy(ctx):
 
     await ctx.send(conspiracies[0])
 
-#####################
-
-# RuneScape Stuff
-
-#####################
-
-@KomediBot.command()
-async def osrswiki(ctx):
-
-    await ctx.send('osrs wiki page for item in ~osrswiki <item>')
-
-@KomediBot.command()
-async def osrsge(ctx):
-
-    await ctx.send('osrs ge price/data for item in ~osrswiki <item>')
-
-@KomediBot.command()
-async def rs3wiki(ctx):
-
-    await ctx.send('rs3 wiki page for item in ~rs3wiki <item>')
-    
-#####################
-
-# Misc
-
-#####################
-
-@KomediBot.command()
-async def learn(ctx, command, role, flavortext):
-
-    
-    
-
-    if command in (LIST_OF_COMMANDS + roles.gameSummonAliases):
-        await ctx.send('That command already exists')
-
-    elif role in roles.roleIDs:
-        await ctx.send('A command for this role already exists')
-
-    else:
-
-        await ctx.send("I'm a stupid robot who can't figure out how long the flavortext is if you don't put it in quotations")
-        
-        gameSummonAliases = roles.gameSummonAliases
-        roleIDs = roles.roleIDs
-        existingflavortext = roles.flavortext
-
-    
-        os.rename('roles.py','roles_ifthisexistssomethingwentwrong.py')
-
-        #await ctx.send(role)
-
-        gameSummonAliases.append(str(command))
-        roleIDs.append(str(role))
-        existingflavortext.append(str(flavortext))
-
-        f = open('roles.py','x')
-        f.write('gameSummonAliases = ["' + '", "'.join([str(item) for item in gameSummonAliases]) + '"]')
-        f.write('\n\n')
-        f.write('roleIDs = ["' + '", "'.join([str(item) for item in roleIDs]) + '"]')
-        f.write('\n')
-        f.write('flavortext = ["' + '", "'.join([str(item) for item in existingflavortext]) + '"]')
-        f.write('\n\n')
-        f.write('gameSummonData = list(zip(roleIDs,flavortext))')
-        f.write('\n\n')
-        f.write('gameSummonDict = dict(zip(gameSummonAliases,gameSummonData))')
-        f.close()
-
-        importlib.reload(roles)
-
-        os.remove('roles_old.py')
-        os.rename('roles_ifthisexistssomethingwentwrong.py','roles_old.py')
-
-        await ctx.send('Me smart now, me know now what ~'+str(roles.gameSummonAliases[-1]) +' means')
-
 @KomediBot.command()
 async def help(ctx,full='0'):
-
     response = '**AVAILABLE COMMANDS:**\n\n'
-    response+= '\n**Fun Stuff:**\n\n'
+    response+= '\n**Music:**\n\n'
+    response+= '**~p <link/search item>** : Plays the audio from <link>, or the audio of the first search result for <search item>. Can also use ~play. NOT IMPLEMENTED\n'
+    response+= '**~s** : Skips the current item in the queue. NOT IMPLEMENTED\n'
+    response+= '**~dc** : Disconnects KomediBot from the voice channel. NOT IMPLEMENTED\n'
+    response+= '**~addq <link/search item>** : Adds the audio from <link/search item> to the queue. NOT IMPLEMENTED\n'
+    response+= '**~showq** : Shows the current queue. NOT IMPLEMENTED\n'
+    response+= '**~clrq** : Clears the current queue except for ongoing audio. NOT IMPLEMENTED\n'
+    response+= '\n**Game Specific:**\n\n'
+    response+= '**~r6op <team> <number>** : Generates <number> operators from <team> (i.e. attack/defence).\n'
+    response+= '**~r <numdice>d<typedice>** : Rolls <numdice>d<typedice> and displays the result (and individual rolls). Can also use ~roll. NOT IMPLEMENTED\n' ### maybe in the future might be worth making a dedicated DnD set of commands?
+    response+= '**~osrswiki <item>** : Returns OSRS wiki page for <item>. NOT IMPLEMENTED\n'
+    response+= '**~osrsge <item>** : Returns OSRS GE information from wiki for <item>. NOT IMPLEMENTED\n'
+    response+= '**~rs3wiki <item>** : Returns RS3 wiki page for <item>. NOT IMPLEMENTED\n'
+    response+= '\n**Misc:**\n\n'
     response+= '**~quote** : Generates a random quote from the BBB Twitter @BBBQuotes1.\n'
     response+= '**~conspiracy** : Generates random conspiracy theory.\n'
-    response+= '**~r6op <team> <number>** : Generates <number> operators from <team> (i.e. attack/defence).\n'
-    response+= '**~roll <numdice><typedice>** : NOT IMPLEMENTED\n' ### maybe in the future might be worth making a dedicated DnD set of commands?
-    response+= '\n**RuneScape Things:**\n\n'
-    response+= '**~osrswiki <item>** : NOT IMPLEMENTED\n'
-    response+= '**~osrsge <item>** : NOT IMPLEMENTED\n'
-    response+= '**~rs3wiki <item>** : NOT IMPLEMENTED\n'
-    response+= '\n**Game Summons:**\n\n'
-    response+= '**~siege** : Tags the Rainbow 6: Siege role.\n'
+    response+= '**~help** : I think you know what it does... <:fuckboi:988374504168390717>'
 
     if full.lower() == 'full' or full == '1':
         full = 1
 
     if full==1:
-
         response+= '\n\n**PASSIVE FUNCTIONS:**\n\n'
         response+= '- Responds to LMAO variants with "KOMEDI" as many times as there are Os.\n'
         response+= '- Automatically converts and messages timezone based on timezone role of sender. NOT IMPLEMENTED\n'
@@ -260,4 +283,3 @@ async def help(ctx,full='0'):
 #######################################################################################################
 
 KomediBot.run(DISCORD_TOKEN)
-
--- roles.py
+++ roles.py
@@ -1,7 +1,7 @@
-gameSummonAliases = ["siege", "valheim", "minecraft", "tabg", "hog", "phasmophobia", "iwakeupwithdickinmymouth", "satisfactory", "irishcunt"]
+gameSummonAliases = ["siege", "valheim", "minecraft", "tabg", "hog", "phasmophobia", "iwakeupwithdickinmymouth", "satisfactory", "irishcunt", "abhi"]
 
-roleIDs = ["<@&755600801853866084>", "<@&809230158615543828>", "<@&755604511871074316>", "<@&882345310712659998>", "<@&865758534368755722>", "<@&882344756041093140>", "<@&755607442326552616>", "<@&988855644683632711>", "<@252224590300971019>"]
-flavortext = ["Do not attempt to board the helicopter.", "viking game time now mufuckaz", "if its vanilla im not playing", "men with large cocks who play video games, unite!", "hog hog hog hog hog hog hog hog hog", "i wanna play basketball", "cocket my league, daddy", "for the glory of lord bezos", "Ding dong the wicked bitch is dead!"]
+roleIDs = ["<@&755600801853866084>", "<@&809230158615543828>", "<@&755604511871074316>", "<@&882345310712659998>", "<@&865758534368755722>", "<@&882344756041093140>", "<@&755607442326552616>", "<@&988855644683632711>", "<@252224590300971019>", "<@261978216930082817>"]
+flavortext = ["Do not attempt to board the helicopter.", "viking game time now mufuckaz", "if its vanilla im not playing", "men with large cocks who play video games, unite!", "hog hog hog hog hog hog hog hog hog", "i wanna play basketball", "cocket my league, daddy", "for the glory of lord bezos", "Ding dong the wicked bitch is dead!", "JAVABOT-8000 ACTIVATED. NEURAL NETWORK ONLINE. TARGETING SYSTEM ONLINE. BRAIN.... OFFLINE."]
 
 gameSummonData = list(zip(roleIDs,flavortext))
 
